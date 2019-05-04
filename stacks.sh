9d954c9bfefecf2fc826a2a2358eab87d5b84b20
tvugeveloper <42783389+tvugeveloper@users.noreply.github.com>
2019-05-04 18:51:48 +0200

    Update stacks.sh

--- stacks.sh
+++ stacks.sh
@@ -1,13 +1,13 @@
 #/bin/bash
 
 #Install and configure Stacks_2.3d
-conda create --override-channels --channel iuc --channel conda-forge --channel bioconda --channel defaults --name stacks_2.3d stacks=2.3d
+/root/anaconda3/bin/conda create --override-channels --channel iuc --channel conda-forge --channel bioconda --channel defaults --name stacks_2.3d stacks=2.3d
 #source activate stacks_2.3d
-conda activate stacks_2.3d
-conda install stacks
-conda install -c r r 
-conda install -c r r-ggplot2
-conda activate stacks_2.3d
+/root/anaconda3/bin/conda activate stacks_2.3d
+/root/anaconda3/bin/conda install stacks
+/root/anaconda3/bin/conda install -c r r 
+/root/anaconda3/bin/conda install -c r r-ggplot2
+/root/anaconda3/bin/conda activate stacks_2.3d
 ln -s /root/anaconda3/envs/stacks_2.3.d/lib/libreadline.so.7.0 /root/anaconda3/envs/stacks_2.3.d/lib/libreadline.so.6
 cd /root/Bafstu
 bash Start_IBEDs_STACK_pipeline.sh
